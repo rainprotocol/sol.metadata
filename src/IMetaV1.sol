cdd9728db9642ed8276d5b3d7df04f5c3bc29a77
thedavidmeister <thedavidmeister@gmail.com>
2023-03-21 13:28:51 +0400

    add subject to metav1

--- src/IMetaV1.sol
+++ src/IMetaV1.sol
@@ -18,9 +18,13 @@ uint64 constant META_MAGIC_NUMBER_V1 = 0xff0a89c674ee7874;
 interface IMetaV1 {
     /// An onchain wrapper to carry arbitrary Rain metadata. Assigns the sender
     /// to the metadata so that tooling can easily drop/ignore data from unknown
-    /// sources.
+    /// sources. As metadata is about something, the subject MUST be provided.
     /// @param sender The msg.sender.
+    /// @param subject The entity that the metadata is about. MAY be the address
+    /// of the emitting contract (as `uint256`) OR anything else. The
+    /// interpretation of the subject is context specific, so will often be a
+    /// hash of some data/thing that this metadata is about.
     /// @param meta Rain metadata V1 compliant metadata bytes.
     /// https://github.com/rainprotocol/specs/blob/main/metadata-v1.md
-    event MetaV1(address sender, bytes meta);
+    event MetaV1(address sender, uint256 subject, bytes meta);
 }
